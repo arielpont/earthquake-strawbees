8fa59acda5faf3390674bd47850e83392f9a0225
Ariel Pont <arielpont@users.noreply.github.com>
2024-10-07 09:04:45 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    speed += 1
+    if (speed > 4) {
+        speed = 1
+    }
+    basic.showNumber(speed)
+})
+let speed = 0
+sb.setServoPosition(sb.servo(SBServo.ServoA), 50)
+speed = 1
+basic.showNumber(speed)
+basic.forever(function () {
+    sb.transitionServoPosition(sb.servo(SBServo.ServoA), 40, 1 / speed, sb.easing(SBEasing.Linear))
+    sb.transitionServoPosition(sb.servo(SBServo.ServoA), 70, 1 / speed, sb.easing(SBEasing.Linear))
+})
