126bb5b5a882632aebaa449129e341fa7dfb6b2f
Ariel Pont <arielpont@users.noreply.github.com>
2024-10-10 09:26:41 -0300

    New speed

--- main.ts
+++ main.ts
@@ -6,10 +6,10 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(speed)
 })
 let speed = 0
-sb.setServoPosition(sb.servo(SBServo.ServoA), 50)
+sb.setServoPosition(sb.servo(SBServo.ServoA), 0)
 speed = 1
 basic.showNumber(speed)
 basic.forever(function () {
-    sb.transitionServoPosition(sb.servo(SBServo.ServoA), 50, 1 / speed, sb.easing(SBEasing.Linear))
-    sb.transitionServoPosition(sb.servo(SBServo.ServoA), 100, 1 / speed, sb.easing(SBEasing.Linear))
+    sb.transitionServoPosition(sb.servo(SBServo.ServoA), 65, 1 / speed, sb.easing(SBEasing.Linear))
+    sb.transitionServoPosition(sb.servo(SBServo.ServoA), 0, 1 / speed, sb.easing(SBEasing.Linear))
 })
